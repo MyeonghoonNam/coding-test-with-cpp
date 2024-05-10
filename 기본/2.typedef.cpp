c610f831b3eb87d6c84a48b6be9623502c0e71f6
MyeonghoonNam <codenmh0822@naver.com>
2024-05-10 14:57:45 +0900

    feat : typedef

---
+++ 기본/2.typedef.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef int i;  // --1
+
+int main() {
+  i a = 1;
+
+  cout << a << '\n';
+
+  return 0;
+}
+
+/*
+  1. typedef <타입> <별칭>
+  타입의 이름을 새로운 별칭으로 정의해 실제 타입이름 대신 사용
+  int 타입을 i라는 새로운 별칭으로 사용
+*/
