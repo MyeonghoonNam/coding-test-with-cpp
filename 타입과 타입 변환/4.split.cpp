c9b593b3010bf6a544261d8fa79794ab5b833e34
MyeonghoonNam <codenmh0822@naver.com>
2024-05-15 13:59:31 +0900

    feat : split performance

--- 타입과 타입 변환/4.split.cpp
+++ 타입과 타입 변환/4.split.cpp
@@ -4,17 +4,37 @@ using namespace std;
 
 typedef long long ll;
 
-vector<string> split(string str, string delimeter) {
-  vector<string> ret;
-  ll pos;
+// 시간복잡도 문제 없을 때의 split 함수
+// vector<string> split(string str, string delimiter) {
+//   vector<string> ret;
+//   ll pos;
 
-  while ((pos = str.find(delimeter)) != string::npos) {
-    string token = str.substr(0, pos);
+//   while ((pos = str.find(delimiter)) != string::npos) {
+//     string token = str.substr(0, pos);
+//     ret.push_back(token);
+//     str.erase(0, pos + delimiter.length());
+//   }
+
+//   ret.push_back(str);
+
+//   return ret;
+// }
+
+// 시간복잡도를 좀 더 개선한 split 함수
+vector<string> split(string str, string delimiter) {
+  vector<string> ret;
+  ll start = 0;
+  ll end = str.find(delimiter);
+
+  while (end != string::npos) {
+    string token = str.substr(start, end - start);
+
     ret.push_back(token);
-    str.erase(0, pos + delimeter.length());
+    start = end + delimiter.length();
+    end = str.find(delimiter, start);
   }
 
-  ret.push_back(str);
+  ret.push_back(str.substr(start));
 
   return ret;
 }
