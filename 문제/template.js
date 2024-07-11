291c7fb2fb9fd757bb9c308703ee09c3f0c7ce41
MyeonghoonNam <codenmh0822@naver.com>
2024-07-11 12:56:40 +0900

    refactor : template 입력값 초기화

--- 문제/template.js
+++ 문제/template.js
@@ -1,15 +1,6 @@
 const fs = require("fs");
 const stdin = (
-  process.platform === "linux"
-    ? fs.readFileSync("/dev/stdin").toString()
-    : `7 7
-1011111
-1110001
-1000001
-1000001
-1000001
-1000001
-1111111`
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : ``
 ).split("\n");
 
 const input = (() => {
