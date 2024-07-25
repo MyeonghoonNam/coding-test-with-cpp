91137aec47af1ebb58640be484a3f4ff2652e800
MyeonghoonNam <codenmh0822@naver.com>
2024-07-25 15:50:43 +0900

    짝짓기 유형 스택 활용

---
+++ 문제/DFS&BFS/17298.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int a[1000004];
+int ret[1000004];
+
+stack<int> stk;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> N;
+
+  memset(ret, -1, sizeof(ret));
+
+  for (int i = 0; i < N; i++) {
+    cin >> a[i];
+
+    while (stk.size() && a[stk.top()] < a[i]) {
+      ret[stk.top()] = a[i];
+      stk.pop();
+    }
+
+    stk.push(i);
+  }
+
+  for (int i = 0; i < N; i++) {
+    cout << ret[i] << " ";
+  }
+
+  return 0;
+}
